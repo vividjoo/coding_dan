9f6ff56e6a111cb020aa9d0766f26432b478ac13
주태윤 <vividjtj@gmail.com>
2023-02-17 01:08:45 +0900

    첫 설정

---
+++ src/test/java/com/example/boots/BootsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.boots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("contextLoads....");
+	}
+
+}
